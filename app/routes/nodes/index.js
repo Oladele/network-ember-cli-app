677338c2d5e5d7502d8214d6d77889df25b169bc
Ola <oladele@gmail.com>
2015-04-03 14:51:40 -0400

    add edges to nodes#index route,template

--- app/routes/nodes/index.js
+++ app/routes/nodes/index.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return this.store.findAll('node');
+    var nodes = this.store.findAll('node');
+    var edges = this.store.findAll('edge');
+    return {
+      nodes: nodes,
+      edges: edges
+    };
   }
 });
