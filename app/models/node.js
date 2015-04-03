5cff82f350b71b9da654ca330a277949b992fb23
Ola <oladele@gmail.com>
2015-04-03 14:06:52 -0400

    implement node FixtureAdapter + fixture data

--- app/models/node.js
+++ app/models/node.js
@@ -1,11 +1,18 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
-  label: DS.attr('string'),
+var Node = DS.Model.extend({
+  label: DS.attr('string')
+});
+
+Node.reopenClass({
   FIXTURES: [
     {id: 1, label: 'Node 1'},
     {id: 2, label: 'Node 2'},
     {id: 3, label: 'Node 3'},
     {id: 4, label: 'Node 4'},
     {id: 5, label: 'Node 5'}
+  ]
 });
+
+
+export default Node
