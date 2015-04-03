a22783b9e1f69c25ae8e52c232d0c9ecf9e699b4
Ola <oladele@gmail.com>
2015-04-03 15:51:14 -0400

    ember g util vis-helpers

---
+++ app/utils/vis-helpers.js
@@ -0,0 +1,3 @@
+export default function visHelpers() {
+  return true;
+}
---
+++ tests/unit/utils/vis-helpers-test.js
@@ -0,0 +1,10 @@
+import visHelpers from '../../../utils/vis-helpers';
+import { module, test } from 'qunit';
+
+module('visHelpers');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = visHelpers();
+  assert.ok(result);
+});
