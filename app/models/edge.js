9cd2e64be3f5cee486fb24c06041db17afc2d81d
Ola <oladele@gmail.com>
2015-04-03 14:43:34 -0400

    add edge fixture data

--- app/models/edge.js
+++ app/models/edge.js
@@ -1,6 +1,17 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+var Edge = DS.Model.extend({
   from: DS.attr(),
   to: DS.attr()
 });
+
+Edge.reopenClass({
+  FIXTURES:  [
+    {id: 1, from: 1, to: 2},
+    {id: 2, from: 1, to: 3},
+    {id: 3, from: 2, to: 4},
+    {id: 4, from: 2, to: 5}
+  ]
+});
+
+export default Edge
