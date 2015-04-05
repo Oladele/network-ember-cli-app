1424b330a00040f470ab8d33b26c06389044dcf0
Ola <oladele@gmail.com>
2015-04-05 08:00:31 -0400

    get nodes in nodes route instead nodes#index route

--- app/routes/nodes/index.js
+++ app/routes/nodes/index.js
@@ -1,12 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(){
-    var nodes = this.store.findAll('node');
-    var edges = this.store.findAll('edge');
-    return {
-      nodes: nodes,
-      edges: edges
-    };
-  }
 });
