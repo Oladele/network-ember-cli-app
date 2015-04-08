014687c1aa69635ea13771c78c51ba51a6744587
Ola <oladele@gmail.com>
2015-04-08 18:22:03 -0400

    ember g util to-linked-list-helper

---
+++ tests/unit/utils/to-linked-list-helper-test.js
@@ -0,0 +1,10 @@
+import toLinkedListHelper from '../../../utils/to-linked-list-helper';
+import { module, test } from 'qunit';
+
+module('toLinkedListHelper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = toLinkedListHelper();
+  assert.ok(result);
+});
