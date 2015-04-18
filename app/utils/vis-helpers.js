5e2e88ee0d492e60eb13938a57738fc1c7f4a8c8
Ola <oladele@gmail.com>
2015-04-18 11:18:18 -0400

    use small methods and remove unnec child edges

--- app/utils/vis-helpers.js
+++ app/utils/vis-helpers.js
@@ -3,14 +3,11 @@ export default function visHelpers() {
 }() //immediately invoked function returns vis obj which is exported
 
 export function visFilter(params){
-  var nodes_vis, edges_vis, level, end_node_id;
+  var nodes_vis, edges_vis, level ;
   // required
   nodes_vis = params.nodes_vis;
   edges_vis = params.edges_vis;
-  
-  // optional (if neither is passed, then nothing will be filtered)
   level = params.level;
-  end_node_id = end_node_id;
 
   // internal
   var filtered_nodes = []
@@ -23,51 +20,42 @@ export function visFilter(params){
 
   nodes_vis.remove(filtered_nodes);
   edges_vis.remove(filtered_edges);
-
-  // console.log("visFilter params:", params);
-  // console.log("visFilter nodes_vis.length:", nodes_vis.length);
-  // console.log("visFilter filtered_nodes.length:", filtered_nodes.length);
-  // console.log("visFilter edges_vis.length:", edges_vis.length);
-  // console.log("visFilter filtered_edges.length:", filtered_edges.length);
-  // window.temp_filtered_nodes = filtered_nodes;
 }
 
-function getNodesAboveLevel(nodes_vis, level, above){
-
-  if (above === true || above === undefined) {
-    var filtered_nodes = nodes_vis.get({
-      filter: function(node){
-        return node.level > level;
-      }
-    });
-  }else{
-    var filtered_nodes = nodes_vis.get({
-      filter: function(node){
-        return node.level <= level;
-      }
-    });
-  }
-
-
+function getNodesAboveLevel(nodes_vis, level){
+  var filtered_nodes = nodes_vis.get({
+    filter: function(node){
+      return node.level > level;
+    }
+  });
   return filtered_nodes;
+}
+
+function getEdgesConnectedToNodes(edges_vis, nodes){
+  var filtered_edges = edges_vis.get({
+    filter: function(edge){
+      return isEdgeConnectedToNodes(edge, nodes)
+    }
+  });
+  return filtered_edges;
+}
+
+function isEdgeConnectedToNodes(edge, nodes){
+  var nodes_ids = getIds(nodes);
+
+  var isEdgeConnectedToNode = isIdInArray(edge.to, nodes_ids);
+  var isEdgeConnectedFromNode = isIdInArray(edge.from, nodes_ids);
+  return (isEdgeConnectedToNode)||(isEdgeConnectedFromNode);
+}
+
+function isIdInArray(id, array){
+  var position = $.inArray(id, array);
+  return (position > -1);
 }
 
 function getIds(nodes_or_edges){
   var ids = nodes_or_edges.map(function(item){
-    return item.id;
+    return parseInt(item.id);
   });
   return ids;
-}
-
-function getEdgesConnectedToNodes(edges_vis, nodes){
-  var filtered_nodes_ids = getIds(nodes);
-
-  var filtered_edges = edges_vis.get({
-    filter: function(edge){
-      var id_str = edge.to.toString();
-      var result = $.inArray(id_str, filtered_nodes_ids);
-      return (result !== -1) ;
-    }
-  });
-  return filtered_edges;
 }
