50b9d5a4a125a1ecc409d19c5a8c29701e55f2e3
Ola <oladele@gmail.com>
2015-04-19 17:54:43 -0400

    add showEqualOrLessThanLevel method

--- app/utils/vis-display-helper.js
+++ app/utils/vis-display-helper.js
@@ -24,21 +24,40 @@ function makeDisplayHelper(nodes_vis, edges_vis){
       };
     },
 
+    showEqualOrLessThanLevel: function(level){
+      var data = this.finderHelper.findEqualOrLessThanLevel(level);
+      this._clearNodesEdges();
+      this._updateNodesEdges(data);
+    },
+
     _showDescendants: function(node_id){
-      var data = this.finderHelper.getDescendants(node_id,2);
+      var data = this.finderHelper.findDescendants(node_id,2);
+      this._updateNodesEdges(data);
+    },
+
+    _updateNodesEdges: function(data){
       this.nodes_vis.update(data.nodes);
       this.edges_vis.update(data.edges);
     },
 
+    _clearNodesEdges: function(){
+      this.nodes_vis.clear();
+      this.edges_vis.clear();
+    },
+
     _hideDescendants: function(node_id){
-      var data = this.finderHelper.getDescendants(node_id);
+      var data = this.finderHelper.findDescendants(node_id);
+      this._removeNodesEdges(data);
+    },
+
+    _removeNodesEdges: function(data){
       this.nodes_vis.remove(data.nodes);
       this.edges_vis.remove(data.edges);
     },
 
     _areDescendantsShowing: function(node_id){
 
-      var descendant_data = this.finderHelper.getDescendants(node_id,1);
+      var descendant_data = this.finderHelper.findDescendants(node_id,1);
       var node_vis = this.nodes_vis.get(node_id);
       var children_edges = this.edges_vis.get({
         filter: function(edge){
